7f76891fe2e406cd5dec34b8e81d7b53a4d4478b
Gabriel Ngandu-Biseba <gabriel.ngandu-biseba@tucrail.be>
2024-08-09 15:40:31 +0200

    Complete widget tests

--- src/Tests/Widget/Widget.test.tsx
+++ src/Tests/Widget/Widget.test.tsx
@@ -1,8 +1,8 @@
-import {render} from "@testing-library/react";
+import {fireEvent, render, screen, waitFor} from "@testing-library/react";
 import {Widget} from "../../widget";
 import React from "react";
 import {WidgetSettings} from "azure-devops-extension-api/Dashboard";
-import {spyWidgetCallBackAccessor} from "../../__mocks__/azure-devops-extension-sdk";
+import {mockGetProject, spyWidgetCallBackAccessor} from "../../__mocks__/azure-devops-extension-sdk";
 import {filledWidgetConfiguration} from "../../__mocks__/Common";
 import {BuildResult, createBuild, mockGetBuilds, mockGetTags} from "../../__mocks__/azure-devops-extension-api/Build";
 
@@ -33,7 +33,8 @@ describe("Widget", () => {
         // @ts-ignore
         const widget = spyWidgetCallBackAccessor as Widget;
 
-        widget.load(args);
+        await widget.load(args);
+        await widget.reload(args);
 
         await delay(1);
 
@@ -68,7 +69,7 @@ describe("Widget", () => {
         const widget = spyWidgetCallBackAccessor as Widget;
 
         await widget.preload(args);
-        widget.load(args);
+        await widget.load(args);
 
         await delay(1);
 
@@ -108,14 +109,18 @@ describe("Widget", () => {
         const widget = spyWidgetCallBackAccessor as Widget;
 
         await widget.preload(args);
-        widget.load(args);
+        await widget.load(args);
+        await widget.reload(args);
 
         await delay(1);
 
 
     })
 
-    test('Widget - Render with filled configuration and pre-selected tag', async() => {
+    it.each([
+        true,
+        false
+    ])('Widget - Render with filled configuration, pre-selected tag and "matchAnyTag" set to %s', async(matchAnyTag) => {
         render(
             <Widget></Widget>
 
@@ -124,6 +129,7 @@ describe("Widget", () => {
         await delay(1);
         let configuration = filledWidgetConfiguration;
         configuration.defaultTag = "tag1";
+        configuration.matchAnyTag = matchAnyTag;
         const args: WidgetSettings = {
             name: "settings",
             size: {
@@ -150,15 +156,97 @@ describe("Widget", () => {
         const widget = spyWidgetCallBackAccessor as Widget;
 
         await widget.preload(args);
-        widget.load(args);
+        await widget.load(args);
 
         await delay(1);
 
 
     })
 
+    test("Widget - Render tag selection dropdown, select a tag then clear them all", async () => {
+        render(
+            <Widget></Widget>
 
+        )
+
+        await delay(1);
+        const args: WidgetSettings = {
+            name: "settings",
+            size: {
+                rowSpan: 3,
+                columnSpan: 3
+            },
+            lightboxOptions: undefined,
+            customSettings: {
+                version: undefined,
+                data: JSON.stringify(filledWidgetConfiguration)
+            }
+        }
+
+        mockGetBuilds.mockReturnValue([
+            createBuild(BuildResult.Succeeded)
+        ]);
+
+        mockGetTags.mockReturnValue([
+            "tag1",
+            "tag2"
+        ])
+
+        // @ts-ignore
+        const widget = spyWidgetCallBackAccessor as Widget;
+
+        await widget.preload(args);
+        await widget.load(args);
+        await widget.reload(args);
+
+        await delay(1);
+
+        await delay(500);
+
+        const tagDropdown = screen.getByRole("button");
+        fireEvent.click(tagDropdown);
+        await delay(500);
+        const tagElementToClick = screen.getByText("tag2", {exact: false});
+        fireEvent.click(tagElementToClick);
+        const clearElement = screen.getByText("Clear", {exact: false});
+        fireEvent.click(clearElement);
+    })
+
+    test("Widget - Render with invalid data", async () => {
+        render(
+            <Widget></Widget>
+
+        )
+
+        await delay(1);
+        const args: WidgetSettings = {
+            name: "settings",
+            size: {
+                rowSpan: 3,
+                columnSpan: 3
+            },
+            lightboxOptions: undefined,
+            customSettings: {
+                version: undefined,
+                data: JSON.stringify(filledWidgetConfiguration)
+            }
+        }
+
+        // @ts-ignore
+        const widget = spyWidgetCallBackAccessor as Widget;
+
+        mockGetProject.mockReturnValue({
+            id: "invalidId",
+            name: "Invalid project"
+        })
+
+        await widget.load(args);
+        await widget.reload(args);
+
+        await delay(1);
+    });
 })
+
 
 
 
